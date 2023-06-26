661aecf70f08025e2e8e6fa86687719f46269cb5
Mr-robot <hackingtest017@gmail.com>
2023-06-26 19:59:48 -0400

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,5 @@
 export default function createReportObject(employeesList) {
-    return {
+  return {
     allEmployees: employeesList,
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
