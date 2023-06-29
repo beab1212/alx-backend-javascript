a94e0c0ac037c8072cf29c40316836d698daee60
Mr-robot <hackingtest017@gmail.com>
2023-06-29 15:52:43 -0400

    remove underscore

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,21 +3,27 @@ export default class Currency {
     this.code = code;
     this.name = name;
   }
-  
-  set code(arg) {
-    this._code = arg;
-  }
 
   get code() {
     return this._code;
   }
 
-  set name(arg) {
-    this._name = arg;
+  set code(code) {
+    if ((typeof code !== 'string'){
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
   }
 
   get name() {
     return this._name;
+  }
+
+  set name(name) {
+    if ((typeof name !== 'string'){
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
   }
 
   displayFullCurrency() {
