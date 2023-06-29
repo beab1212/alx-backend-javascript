f75158f219328b261ce1370eda06a23f02cc11db
Mr-robot <hackingtest017@gmail.com>
2023-06-29 15:33:33 -0400

    add semicolon

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,5 @@
-import classRoom from './0-classroom';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [
-    new classRoom(19),
-    new classRoom(20),
-    new classRoom(34)
-  ];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
