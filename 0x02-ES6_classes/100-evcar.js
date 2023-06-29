a35a11db89b91806cb2ea4bd8e739ff3fdcd301f
Mr-robot <hackingtest017@gmail.com>
2023-06-29 16:37:53 -0400

    disable eslint

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,13 @@
-/* eslint-disable */
 import Car from './10-car';
 
-export default class EVCar extends Car {
+export default class EVcar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
   cloneCar() {
-    return new Car();
+    const CloneCarObj = super.constructor || super[Symbol.species];
+    return new CloneCarObj();
   }
+}
