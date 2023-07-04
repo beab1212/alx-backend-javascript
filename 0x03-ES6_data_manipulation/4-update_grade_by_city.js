d21eeeed417026f64f2e19493db92d5214ce2c1f
Mr-robot <hackingtest017@gmail.com>
2023-07-04 14:47:12 -0400

    space fix

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,9 +2,7 @@ export default function updateStudentGradeByCity(studs, city, newGrades) {
   return studs.filter((value) => {
     return value.location === city;
   }).map((value) => {
-    let grade = newGrades.filter((val) => {
-      val.studentId === value.id
-    });
+    let grade = newGrades.filter((val) => val.studentId === value.id);
     if (grade.length !== 0) {
       value['grade'] = grade[0].grade;
       return value;
