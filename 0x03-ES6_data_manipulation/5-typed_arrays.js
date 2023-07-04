f0e66a31684e1ccc54dfda57fbeea1ad35379714
Mr-robot <hackingtest017@gmail.com>
2023-07-04 14:54:07 -0400

    change error handling

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw new Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer, 0, length);
-  try {
-    const Int8TypedArray = new Int8Array(buffer);
-    Int8TypedArray[position] = value;
-  } catch {
-    throw new Error('Position outside range');
-  }
+  const Int8TypedArray = new Int8Array(buffer);
+  Int8TypedArray[position] = value;
   return view;
 }
