00c7fa8fc5378c93c32faf93bfc7bea7b27ffe04
Mr-robot <hackingtest017@gmail.com>
2023-07-04 15:11:12 -0400

    hot fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,10 @@
 const weakMap = new WeakMap();
 
 export default function queryAPI(endpoint) {
-  let total  = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, total += 1);
-  if(total >= 5)
+  const total  = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, total -= -1);
+  if(total >= 5) {
     throw new Error('Endpoint load is high');
+  }
   return total;
 }
