33070b101b4e4d72625d67a8f3e581bd9cd4360b
Mr-robot <hackingtest017@gmail.com>
2023-07-04 15:00:43 -0400

    fix iteration method

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
   const length = startString.length;
   const result = [];
-  if (startString === '') {
-    return result.join('-');
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
   }
-  for (const val of set) {
-    if (val.startsWith(startString))
-      result.push(val.slice(length))
-  }
+  set.forEach((value) => {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(length));
+    }
+  });
   return result.join('-');
 }
