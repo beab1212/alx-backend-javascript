8c1646e3e78c196c4b27a746d21e3c37f76ae70f
Mr-robot <hackingtest017@gmail.com>
2023-07-04 15:07:46 -0400

    change error handling method

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,10 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   map.forEach((value, key) => {
     if (value === 1) {
-      try {
-        map.set(key, 100);
-      } catch {
-        throw new Error('Cannot process');
-      }
+      map.set(key, 100);
     }
   });
 }
