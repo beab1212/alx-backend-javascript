b9f58e9ad844b93b52554f6db99500717d4376d2
Mr-robot <hackingtest017@gmail.com>
2024-09-17 14:56:30 +0300

    init

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  // const chunk = process.stdin.read();
+
+  // if (chunk) {
+  //   process.stdout.write(`Your name is: ${chunk}`);
+  // }
+  process.stdout.write(`Your name is: ${data}`);
+  process.exit();
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
