5e587cbdc45dc95b4751778bcbb4567569b519fa
Mr-robot <hackingtest017@gmail.com>
2024-09-18 21:08:19 +0300

    task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -7,6 +7,22 @@ app.route('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+app.all('*', (req, res) => {
+  const data = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Error</title>
+    </head>
+    <body>
+    <pre>Cannot GET /lskdlskd</pre>
+    </body>
+    </html>
+  `;
+  res.send(data);
+});
+
 app.listen(1234);
 
 module.exports = app;
