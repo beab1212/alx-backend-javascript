48fc308e73c6fb4020290061a6dd234ad5d12ce7
Mr-robot <hackingtest017@gmail.com>
2024-09-18 21:11:28 +0300

    task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,27 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.route('/', (req, res) => {
-  res.status(200);
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.all('*', (req, res) => {
-  const data = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="utf-8">
-    <title>Error</title>
-    </head>
-    <body>
-    <pre>Cannot GET /lskdlskd</pre>
-    </body>
-    </html>
-  `;
-  res.send(data);
+app.listen(1245, () => {
+  console.log('Server listening');
 });
-
-app.listen(1234);
 
 module.exports = app;
