6d00e89235cecb3d271f3df887a7b6544006a59b
Mr-robot <hackingtest017@gmail.com>
2024-09-20 09:10:50 +0300

    add extra test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -15,4 +15,20 @@ describe('calculateNumber method test', () => {
     const result = calculateNumber('1', '3');
     assert.strictEqual(result, 4);
   });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(1.6, 2.7);
+    assert.strictEqual(res, 5);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(0, 0);
+    assert.strictEqual(res, 0);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(-1.6, -1.7);
+    assert.strictEqual(res, -4);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(-1.4, -1.3);
+assert.strictEqual(res, -2);
+  });
 });
