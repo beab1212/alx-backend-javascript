c07df4f3692d310b3368324abe6a905b912fa16a
Mr-robot <hackingtest017@gmail.com>
2024-09-20 16:27:11 +0300

    add extra test cases

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,27 @@ const { expect } = require('chai');
 const { describe, it } = require('mocha');
 const request = require('request');
 
+describe('Index page', () => {
+  it('test status code', (done) => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('test response content', (done) => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+  it('test response content length', (done) => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      expect(res.headers['content-length']).to.equal('29');
+      done();
+    });
+  });
+});
+
 describe('cart page', () => {
   it('test status code with string id', (done) => {
     request.get('http://localhost:7865/cart/hello', (err, res, body) => {
