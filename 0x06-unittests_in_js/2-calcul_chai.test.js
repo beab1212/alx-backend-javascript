02bfe61e7e9d8f2a791c5dc2af9046fca51fc346
Mr-robot <hackingtest017@gmail.com>
2024-09-20 10:07:33 +0300

    fix test cases

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,46 +3,45 @@ const { expect } = require('chai');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber method test', () => {
-  it('integer number test for SUM', () => {
-    const result = calculateNumber('SUM', 5, 3);
-    expect(result).to.equal(8);
-  });
-  it('float number test for SUM', () => {
-    const result = calculateNumber('SUM', 5.3, 3.6);
-    expect(result).to.equal(9);
-  });
-  it('string number test for SUM', () => {
-    const result = calculateNumber('SUM', '5.3', '3.6');
-    expect(result).to.equal(9);
-  });
-
-  it('integer number test for SUBTRACT', () => {
-    const result = calculateNumber('SUBTRACT', 5, 3);
-    expect(result).to.equal(2);
-  });
-  it('float number test for SUBTRACT', () => {
-    const result = calculateNumber('SUBTRACT', 5.3, 3.6);
-    expect(result).to.equal(1);
-  });
-  it('string number test for SUBTRACT', () => {
-    const result = calculateNumber('SUBTRACT', '5.3', '3.6');
-    expect(result).to.equal(1);
+  describe('test for SUM', () => {
+    it('integer number test for SUM', () => {
+      expect(calculateNumber('SUM', 5, 3)).to.equal(8);
+    });
+    it('float number test for SUM', () => {
+      expect(calculateNumber('SUM', 5.3, 3.6)).to.equal(9);
+    });
+    it('string number test for SUM', () => {
+      expect(calculateNumber('SUM', '5.3', '3.6')).to.equal(9);
+    });
   });
 
-  it('integer number test for DIVIDE', () => {
-    const result = calculateNumber('DIVIDE', 5, 3);
-    expect(result).to.equal(1.6666666666666667);
+  describe('test for SUBTRACT', () => {
+    it('integer number test for SUBTRACT', () => {
+      expect(calculateNumber('SUBTRACT', 5, 3)).to.equal(2);
+    });
+    it('float number test for SUBTRACT', () => {
+      expect(calculateNumber('SUBTRACT', 5.3, 3.6)).to.equal(1);
+    });
+    it('string number test for SUBTRACT', () => {
+      expect(calculateNumber('SUBTRACT', '5.3', '3.6')).to.equal(1);
+    });
   });
-  it('float number test for DIVIDE', () => {
-    const result = calculateNumber('DIVIDE', 5.3, 3.6);
-    expect(result).to.equal(1.25);
-  });
-  it('string number test for DIVIDE', () => {
-    const result = calculateNumber('DIVIDE', '5.3', '3.6');
-    expect(result).to.equal(1.25);
-  });
-  it('division by zero test for DIVIDE', () => {
-    const result = calculateNumber('DIVIDE', 5.3, 0);
-    expect(result).to.equal('Error');
+
+  describe('test for DIVIDE', () => {
+    it('integer number test for DIVIDE', () => {
+      expect(calculateNumber('DIVIDE', 5, 3)).to.equal(1.6666666666666667);
+    });
+    it('float number test for DIVIDE', () => {
+      const result = calculateNumber('DIVIDE', 5.3, 3.6);
+      expect(result).to.equal(1.25);
+    });
+    it('string number test for DIVIDE', () => {
+      const result = calculateNumber('DIVIDE', '5.3', '3.6');
+      expect(result).to.equal(1.25);
+    });
+    it('division by zero test for DIVIDE', () => {
+      const result = calculateNumber('DIVIDE', 5.3, 0);
+      expect(result).to.equal('Error');
+    });
   });
 });
