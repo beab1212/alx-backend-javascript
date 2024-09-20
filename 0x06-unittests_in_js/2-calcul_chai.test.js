ea3d094f9a254ad2b8fe64ca4a5b4e6408e2cdbf
Mr-robot <hackingtest017@gmail.com>
2024-09-20 10:42:29 +0300

    fix test cases

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,7 +3,7 @@ const {describe, it} = require("mocha");
 const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber", function() {
-    describe("SUM", function() {
+    describe("test for SUM", function() {
 	it("checking if numbers round", function() {
 	    expect(calculateNumber("SUM", 1, 2)).to.equal(3);
 	});
@@ -11,7 +11,7 @@ describe("calculateNumber", function() {
 	    expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
 	});
     });
-    describe("SUBTRACT", function() {
+    describe("test for SUBTRACT", function() {
 	it("checking if numbers round", function() {
 	    expect(calculateNumber("SUBTRACT", 1.4, 2.3)).to.equal(-1);
 	});
@@ -22,7 +22,7 @@ describe("calculateNumber", function() {
 	    expect(calculateNumber("SUBTRACT", -4.9, -2.7)).to.equal(-2);
 	});
     });
-    describe("DIVIDE", function() {
+    describe("test for DIVIDE", function() {
 	it("checking if numbers round", function() {
 	    expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
 	});
